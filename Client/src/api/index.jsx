1f7cc5eebe62407d23b9adf39d8d15d76ef7d6cd
MoDMoS <koonmos1@gmail.com>
2023-03-20 10:45:10 +0700

    test api

--- Client/src/api/index.jsx
+++ Client/src/api/index.jsx
@@ -1,3 +1,15 @@
-import axios from 'axios'
+import axios from 'axios';
+import {REACT_APP_API_URL} from '@env';
 
-const LoginAPI = async ()
+const RegisterAPI = async (email, id) => {
+  const url = REACT_APP_API_URL + '/api/regis';
+  console.log(url);
+
+  return axios.post(url, {email: email, id: id} );
+};
+
+const Service = {
+  RegisterAPI,
+};
+
+export default Service;
