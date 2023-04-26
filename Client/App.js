8389f41a5c4b9f683e41a57664965a346224def4
MoDMoS <koonmos1@gmail.com>
2023-04-26 09:07:05 +0700

    Add mock location

--- Client/App.js
+++ Client/App.js
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import JailMonkey from 'jail-monkey';
 import {Alert, Location} from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
 
 import Root from './src/Root';
 
@@ -9,7 +10,24 @@ export default function App() {
 
   useEffect(() => {
     _handleJail();
+    isMockLocationEnabled().then(mockEnabled => {
+      if (mockEnabled) {
+        console.log('Mock locations are enabled');
+      } else {
+        console.log('Mock locations are not enabled');
+      }
+    });
   }, []);
+
+  const isMockLocationEnabled = async () => {
+    try {
+      const provider = await Geolocation.getCurrentProvider();
+      return provider.locationProvider === Geolocation.LOCATION_PROVIDER.MOCK_PROVIDER;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  };
 
   const _handleJail = () => {
     if (!__DEV__ && JailMonkey.isJailBroken()) {
